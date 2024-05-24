80789fb3888fc9ae11386f3c1bcfd8d030cfe633
Luke <luke.wyatt21@gmail.com>
2024-05-24 03:51:35 +0100

    Small text change

--- site/src/NoCamera/index.jsx
+++ site/src/NoCamera/index.jsx
@@ -6,7 +6,7 @@ const NoCamera = ({ error }) => {
         <div className="error-view">
             <div className="container">
                 <h1 className="title">Sorry</h1>
-                <h2 className="camera-required">This app only works on mobile devices with a camera</h2>
+                <h2 className="camera-required">This app only works on mobile devices</h2>
 
                 <div className="contact">
                     <p>Need help? Want to talk?</p>
